236f947b790d203dc3b95b89aa937b8edbc1212f
david putman <davidputman@davidputman.com>
2018-11-21 07:40:12 +0100

    rebase

--- Bdd/Calculator.feature
+++
@@ -1,5 +0,0 @@
-﻿Feature: Calculator
-	In order to avoid silly mistakes
-	As a math idiot
-	I want to be told the sum of two numbers
-
--- TheQueue/TheQueueTests.cs
+++ TheQueue/TheQueueTests.cs
@@ -1,13 +1,43 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace TheQueue
 {
-    [TestFixture]
     public class TheQueueTests
     {
         [Test]
-        public void SanityTest()
+        public void NewQueueHasNoItems()
         {
+            TddQueue queue = new TddQueue();
+            Assert.AreEqual(0, queue.Count);
+        }
+
+        [Test]
+        public void AddingItemIncreasesCount()
+        {
+            TddQueue queue = new TddQueue();
+            queue.Enqueue(43);
+            Assert.AreEqual(1, queue.Count);
+        }
+
+        [Test]
+        public void AddingTooManyItemsThrowsException()
+        {
+            var queue = new TddQueue();
+            Assert.Throws<ArgumentException>(() => queue.Enqueue(37));
+        }
+    }
+
+    internal class TddQueue
+    {
+        public int Count
+        {
+            get; set;
+        }
+
+        public void Enqueue(int v)
+        {
+            ++Count;
         }
     }
 }
