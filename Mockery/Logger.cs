adf1cbad8f317adbd3c5bb87c001dd2ec0f970a0
davidputman <davidputman@davidputman.com>
2019-11-07 13:32:27 +0000

    renamed spudulike

---
+++ Mockery/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace Mockery
+{
+    public class Logger
+    {
+        private readonly IFileWrapper file;
+
+        public Logger(IFileWrapper fileWrapper)
+        {
+            file = fileWrapper;
+        }
+
+        public int Status { get; private set; }
+
+        public void Save(string textToSave)
+        {
+             file.WriteAllText("spudfile.txt", "log: " + textToSave);
+        }
+    }
+}
